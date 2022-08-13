becd579680b46062fe6e8548f3b204f0cb503730
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-13 04:45:57 +0300

    домашнее задание

---
+++ Z13/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+//что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+int N;
+Console.WriteLine("Enter N: ");
+N = int.Parse(Console.ReadLine().ToString());
+if (N.ToString().Length > 2)
+{
+    while (N.ToString().Length > 3)
+        N /= 10;
+    Console.WriteLine(N % 10);
+}
+else
+    Console.WriteLine("No third digit!");
---
+++ Z15/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+6 -> да 
+7 -> да 
+1 -> нет
+
+                                Первое решение задачи
+
+Console.Write("Enter the number of the day of the week: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if(dayNumber == 1)
+{
+    Console.WriteLine("1 -> No");
+}
+if(dayNumber == 2)
+{
+    Console.WriteLine("2 -> No");
+}
+if(dayNumber == 3)
+{
+    Console.WriteLine("3 -> No");
+}
+if(dayNumber == 4)
+{
+    Console.WriteLine("4 -> No");
+}
+if(dayNumber == 5)
+{
+    Console.WriteLine("5 -> No");
+}
+if(dayNumber == 6)
+{
+    Console.WriteLine("6 -> Yes");
+}
+if(dayNumber == 7)
+{
+    Console.WriteLine("7 -> Yes");
+}
+
+                                    Второе решение задачи
+*/
+
+Console.Write("Enter the number of the day of the week: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+void WeeklyTest(int dayNum)
+{
+    if (dayNum == 6 || dayNum == 7)
+    {
+        Console.WriteLine("Weekend"); //Выходной
+    }
+    else if (dayNum < 1 || dayNum > 7)
+    {
+        Console.WriteLine("Is this exactly the day of the week?"); //Это точно день недели?
+    }
+    else Console.WriteLine("The day is not a day off"); //День не выходной
+}
+WeeklyTest(dayNum);
