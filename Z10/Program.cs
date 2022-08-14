0a43b13dac0f18dbd65de3a6e9f1407b4b126d4f
Vladimir <108830774+Vladimir33164@users.noreply.github.com>
2022-08-14 11:38:58 +0300

    Update Program.cs

--- Z10/Program.cs
+++ Z10/Program.cs
@@ -15,3 +15,18 @@ Console.WriteLine("The second digit of this number -> " + snumber[1]);
 
 string number = Console.ReadLine();
 Console.WriteLine(number[1]);
+
+                              //Numeric variant (via while)
+
+int a;
+int result;
+Console.Write("Enter a three-digit number: ");
+a = Convert.ToInt32(Console.ReadLine());
+while ((a > 999) || (a < 100))
+{
+   Console.Write("Error! Enter a number from 100 to 999: ");
+   a = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine();
+}
+result = (a / 10) % 10;
+Console.WriteLine(result);
